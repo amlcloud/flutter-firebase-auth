adf5b2335bdcb04b3ea0432e6c9bceff06a6e346
kovarny <sivochkin@gmail.com>
2023-02-13 21:38:47 +1100

    profile photo

--- lib/show_edit_profile.dart
+++ lib/show_edit_profile.dart
@@ -1,52 +1,63 @@
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:login/profile_photo_editor.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// void showEditProfileDialog(BuildContext context, WidgetRef ref) {
-//   showDialog(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return AlertDialog(
-//             title: Text("Profile"),
-//             content: SizedBox(
-//                 height: 200.0, // Change as per your requirement
-//                 width: 400.0, // Change as per your requirement
-//                 child: Column(
-//                     mainAxisSize: MainAxisSize.max,
-//                     mainAxisAlignment: MainAxisAlignment.center,
-//                     crossAxisAlignment: CrossAxisAlignment.stretch,
-//                     children: [
-//                       Expanded(child: ProfilePhotoEditor()),
-//                       // ...ref
-//                       //     .watch(docSP(
-//                       //         'userInfo/${FirebaseAuth.instance.currentUser?.uid}'))
-//                       //     .when(
-//                       //         data: (userDoc) => [
-//                       //               Text(userDoc.data()!['email']),
-//                       //               FFLTTextEdit(userDoc, 'name', 'user name',
-//                       //                   'edit user name',
-//                       //                   key: Key(userDoc.id)),
-//                       //             ],
-//                       //         loading: () => [Loader()],
-//                       //         error: (e, s) => [ErrorWidget(e)])
-//                     ])),
-//             actions: <Widget>[
-//               ElevatedButton(
-//                   key: Key('sign_out_btn'),
-//                   child: Text("Sign Out"),
-//                   onPressed: () {
-//                     //Navigator.of(context).popUntil(ModalRoute.withName('/'));
-//                     // Navigator.of(context).pushNamed(LoginPage.route);
-//                     Navigator.of(context).pop();
-//                     FirebaseAuth.instance.signOut();
-//                   }),
-//               ElevatedButton(
-//                 child: Text("Close"),
-//                 onPressed: () {
-//                   Navigator.of(context).pop();
-//                 },
-//               )
-//             ]);
-//       });
-// }
+void showEditProfileDialog(BuildContext context, WidgetRef ref) {
+  Widget buildPhoto(BuildContext context, WidgetRef ref) => Center(
+      child: FirebaseAuth.instance.currentUser?.photoURL == null
+          ? Icon(Icons.person)
+          : CircleAvatar(
+              radius: 50,
+              backgroundImage:
+                  Image.network(FirebaseAuth.instance.currentUser!.photoURL!)
+                      .image));
+
+  showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+            title: Text("User Profile"),
+            content: SizedBox(
+                height: 200.0, // Change as per your requirement
+                width: 400.0, // Change as per your requirement
+                child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(child: buildPhoto(context, ref)),
+                      Text(
+                          FirebaseAuth.instance.currentUser!.displayName ?? ''),
+                      Text(FirebaseAuth.instance.currentUser!.uid)
+                      // ...ref
+                      //     .watch(docSP(
+                      //         'userInfo/${FirebaseAuth.instance.currentUser?.uid}'))
+                      //     .when(
+                      //         data: (userDoc) => [
+                      //               Text(userDoc.data()!['email']),
+                      //               FFLTTextEdit(userDoc, 'name', 'user name',
+                      //                   'edit user name',
+                      //                   key: Key(userDoc.id)),
+                      //             ],
+                      //         loading: () => [Loader()],
+                      //         error: (e, s) => [ErrorWidget(e)])
+                    ])),
+            actions: <Widget>[
+              ElevatedButton(
+                  key: Key('sign_out_btn'),
+                  child: Text("Sign Out"),
+                  onPressed: () {
+                    //Navigator.of(context).popUntil(ModalRoute.withName('/'));
+                    // Navigator.of(context).pushNamed(LoginPage.route);
+                    Navigator.of(context).pop();
+                    FirebaseAuth.instance.signOut();
+                  }),
+              ElevatedButton(
+                child: Text("Close"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ]);
+      });
+}
