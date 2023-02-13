35f0cc5fb846a4c136bd9b24ddd09ecd1b329ec6
kovarny <sivochkin@gmail.com>
2023-02-13 22:10:38 +1100

    user profile on click

--- lib/user_avatar.dart
+++ lib/user_avatar.dart
@@ -1,16 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth/show_edit_profile.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UserAvatar extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return FirebaseAuth.instance.currentUser?.photoURL == null
-        ? Icon(Icons.person)
-        : CircleAvatar(
-            radius: 50,
-            backgroundImage:
-                Image.network(FirebaseAuth.instance.currentUser!.photoURL!)
-                    .image);
+    return GestureDetector(
+        onTap: () => showEditProfileDialog(context, ref),
+        child: FirebaseAuth.instance.currentUser?.photoURL == null
+            ? Icon(Icons.person)
+            : CircleAvatar(
+                radius: 50,
+                backgroundImage:
+                    Image.network(FirebaseAuth.instance.currentUser!.photoURL!)
+                        .image));
   }
 }
