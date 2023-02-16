f4edac5a28510c19da43cd9e196ceae0457d77d2
kovarny <sivochkin@gmail.com>
2023-02-16 12:57:14 +1100

    chip and avatar

--- lib/user_avatar.dart
+++ lib/user_avatar.dart
@@ -1,19 +1,17 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase_auth/show_edit_profile.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class UserAvatar extends ConsumerWidget {
+class UserAvatar extends StatelessWidget {
+  final String? photoUrl;
+  const UserAvatar(
+    this.photoUrl, {
+    super.key,
+  });
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return GestureDetector(
-        onTap: () => showEditProfileDialog(context, ref),
-        child: FirebaseAuth.instance.currentUser?.photoURL == null
-            ? Icon(Icons.person)
-            : CircleAvatar(
-                radius: 50,
-                backgroundImage:
-                    Image.network(FirebaseAuth.instance.currentUser!.photoURL!)
-                        .image));
+  Widget build(BuildContext context) {
+    return photoUrl == null
+        ? Icon(Icons.person)
+        : CircleAvatar(
+            radius: 50, backgroundImage: Image.network(photoUrl!).image);
   }
 }
